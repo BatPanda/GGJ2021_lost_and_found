3566ce3577117c5a32550c0fc8076373925a52bf
BatPanda <Agentekul@googlemail.com>
2021-01-30 19:07:42 +0000

    Intergrated items and map stuff
    The itemdata GO needs to be populated still!

--- Project/Assets/ItemStuff/ItemHandler.cs
+++ Project/Assets/ItemStuff/ItemHandler.cs
@@ -11,11 +11,34 @@ public class ItemHandler : MonoBehaviour
 
     public List<GameObject> spawn_locations;
 
-    public GameObject item_prefab; 
+    public GameObject item_prefab;
+
+    private System.Random rng = new System.Random(Guid.NewGuid().GetHashCode());
+
+    public void Shuffle<T>(IList<T> list) {
+        int n = list.Count;
+        while (n > 1) {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
 
     private void Start() {
+
+        List < ItemStruct > shuffled_items = items;
+
+        Shuffle(shuffled_items);
+
+        int iter = 0;
+
         foreach(GameObject element in spawn_locations) {
-            Instantiate(item_prefab, element.transform.position, Quaternion.identity);
+            GameObject go = Instantiate(item_prefab, element.transform.position, Quaternion.identity);
+            go.GetComponent<Item>().item_data = shuffled_items[iter];
+
+            iter++;
         }
     }
 }
