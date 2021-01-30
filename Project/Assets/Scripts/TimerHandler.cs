c4ee5ffb8c8b6577910375a6c357c08c8719c910
Zoe <zoerowbotham1@gmail.com>
2021-01-30 16:12:49 +0000

    Timer and GameOver

---
+++ Project/Assets/Scripts/TimerHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerHandler : MonoBehaviour
+{
+    [SerializeField] private Image m_coutdownImage = default;
+    [SerializeField] private TextMeshProUGUI m_countdownText = default;
+
+    [SerializeField] private int m_secondsUntilGameOver = 120;
+    public UnityEvent OnGameOver;
+
+    private int m_secondsLeft;
+
+    private void Awake()
+    {
+        m_secondsLeft = m_secondsUntilGameOver;
+        UpdateDisplay();
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            m_secondsLeft--;
+            UpdateDisplay();
+
+            if (m_secondsLeft <= 0)
+            {
+                OnGameOver.Invoke();
+                break;
+            }
+        }
+    }
+
+    private void UpdateDisplay()
+    {
+        m_countdownText.text = $"{(m_secondsLeft / 60).ToString("00")}:{(m_secondsLeft % 60).ToString("00")}";
+        m_coutdownImage.fillAmount = m_secondsLeft / (float)m_secondsUntilGameOver;
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
